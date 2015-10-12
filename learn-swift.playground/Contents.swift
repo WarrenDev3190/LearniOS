4c7ea930c3e21f04b4b314ff6c182c38f37754a4
WarrenDev3190 <warrendev3190@gmail.com>
2015-10-12 13:11:15 -0500

    initial commit

---
+++ learn-swift.playground/Contents.swift
@@ -0,0 +1,26 @@
+//Lecture 1 Demo
+
+//This demonstrates how to use vairables,
+//constants, and operators in Swift.
+
+import UIKit  // ignore for now
+
+var str = "Hello, playground"
+
+
+
+// var identifier = initiaValue
+
+var thisYear = 2015 //Int
+
+thisYear = 2016
+
+var courseName = "Introduction to Swift" //String
+var language = "Swift" //String
+
+courseName = "Programming is really fun"
+
+var number = 10.5 //Double
+var programmingIsFun = true //Bool
+
+// TODO: change the number value into 20.5
